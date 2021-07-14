8db3018586922a786b25b1d491525e36713c759e
hardikkumar <hardikkumar@datazoom.io>
2021-07-14 13:42:33 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
+  const responseFacebook = (response: any) => {
+    console.log(response);
+  }
+  const componentClicked = (response: any) => {
+    console.log(response);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Welcome here
+    <FacebookLogin
+    appId="2633424136966343"
+    autoLoad={false}
+    fields="name,email,picture"
+    onClick={componentClicked}
+    callback={responseFacebook} />
     </div>
   );
 }

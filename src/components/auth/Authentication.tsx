d8ae96e0ff749d2d7197b69da7807ee8e9f10107
hardikkumar <hardikkumar@datazoom.io>
2021-07-19 11:35:58 -0500

    Adding authentication

---
+++ src/components/auth/Authentication.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Authentication() {
+  return null;
+}
+
+export default Authentication;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,12 @@ import Link from '@material-ui/core/Link';
 import Navigator from './Navigator';
 import HomePage from '../home/HomePage';
 import Header from './Header';
+import Keycloak from 'keycloak-js';
 import { styles, theme, drawerWidth } from './LayoutTheme';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Config } from '../../config/Config';
+import { AuthClientError, AuthClientEvent } from '@react-keycloak/core';
 
 function Copyright() {
   return (
@@ -32,38 +37,67 @@ function Layout(props: PaperbaseProps) {
   const { classes } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
+  const keycloak = Keycloak({
+    url: `${Config.url.KEYCLOAK_BASE_URL}/auth`,
+    realm: 'company-services',
+    clientId: 'saleseaze-app'
+  });
+
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+  const loadingComponent = <div>Keycloak is loading</div>;
+  const initOptions = { pkceMethod: 'S256' };
+  const handleOnEvent = async (
+    eventType: AuthClientEvent,
+    error?: AuthClientError
+  ) => {
+    alert(eventType);
+    if (eventType === 'onAuthSuccess') {
+      if (keycloak.authenticated) {
+        console.log(keycloak);
+        alert('Hey authencation done');
+      }
+    }
+  };
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <CssBaseline />
-        <nav className={classes.drawer}>
-          <Hidden smUp implementation="js">
-            <Navigator
-              PaperProps={{ style: { width: drawerWidth } }}
-              variant="temporary"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-            />
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Navigator PaperProps={{ style: { width: drawerWidth } }} />
-          </Hidden>
-        </nav>
-        <div className={classes.app}>
-          <Header onDrawerToggle={handleDrawerToggle} />
-          <main className={classes.main}>
-            <HomePage />
-          </main>
-          <footer className={classes.footer}>
-            <Copyright />
-          </footer>
-        </div>
-      </div>
-    </ThemeProvider>
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={initOptions}
+      LoadingComponent={loadingComponent}
+      onEvent={(event, error) => handleOnEvent(event, error)}
+    >
+      <Router>
+        <ThemeProvider theme={theme}>
+          <div className={classes.root}>
+            <CssBaseline />
+            <nav className={classes.drawer}>
+              <Hidden smUp implementation="js">
+                <Navigator
+                  PaperProps={{ style: { width: drawerWidth } }}
+                  variant="temporary"
+                  open={mobileOpen}
+                  onClose={handleDrawerToggle}
+                />
+              </Hidden>
+              <Hidden xsDown implementation="css">
+                <Navigator PaperProps={{ style: { width: drawerWidth } }} />
+              </Hidden>
+            </nav>
+            <div className={classes.app}>
+              <Header onDrawerToggle={handleDrawerToggle} />
+              <main className={classes.main}>
+                <HomePage />
+              </main>
+              <footer className={classes.footer}>
+                <Copyright />
+              </footer>
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
+    </ReactKeycloakProvider>
   );
 }
 

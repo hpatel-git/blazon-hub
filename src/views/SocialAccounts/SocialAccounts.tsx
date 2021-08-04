a745e0b486361fd3d105d506fe0f966d93f6c96a
hardikkumar <hardikkumar@datazoom.io>
2021-08-03 23:34:07 -0500

    Adding CICD

--- src/views/SocialAccounts/SocialAccounts.tsx
+++ src/views/SocialAccounts/SocialAccounts.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 // @material-ui/core components
-import withStyles from '@material-ui/core/styles/withStyles';
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import AddIcon from '@material-ui/icons/Add';
 // core components
 import GridItem from '../../components/Grid/GridItem';
 import GridContainer from '../../components/Grid/GridContainer';
@@ -8,11 +15,69 @@ import Table from '../../components/Table/Table';
 import Card from '../../components/Card/Card';
 import CardHeader from '../../components/Card/CardHeader';
 import CardBody from '../../components/Card/CardBody';
-import { createStyles } from '@material-ui/core';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import ReactFacebookLogin, {
+  ReactFacebookFailureResponse,
+  ReactFacebookLoginInfo
+} from 'react-facebook-login';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import CloseIcon from '@material-ui/icons/Close';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  IconButton,
+  Slide,
+  Tooltip,
+  Typography
+} from '@material-ui/core';
 import { useGetMyProfileQuery } from '../../api/saleseazeApi';
-import Info from '../../components/Typography/Info';
+import { TransitionProps } from '@material-ui/core/transitions/transition';
+
+const connectToSocialAccountsStyle = (theme: Theme) =>
+  createStyles({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2)
+    },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500]
+    }
+  });
+
+export interface DialogTitleProps
+  extends WithStyles<typeof connectToSocialAccountsStyle> {
+  id: string;
+  children: React.ReactNode;
+  onClose: () => void;
+}
 
 const styles = createStyles({
+  facebookIconStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    '& .kep-login-facebook.metro': {
+      width: '100%',
+      borderRadius: 2,
+      fontSize: '14px',
+      fontWeight: 500,
+      textTransform: 'none',
+      display: 'inline-flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      paddingRight: 10,
+      '& svg': {
+        margin: '10px 10px 10px 10px'
+      }
+    }
+  },
   cardCategoryWhite: {
     '&,& a,& a:hover,& a:focus': {
       color: 'rgba(255,255,255,.62)',
@@ -24,6 +89,15 @@ const styles = createStyles({
     '& a,& a:hover,& a:focus': {
       color: '#FFFFFF'
     }
+  },
+  cardAction: {
+    float: 'right',
+    color: '#FFFFFF',
+    marginTop: '0px'
+  },
+  cardTitle: {
+    float: 'left',
+    color: '#FFFFFF'
   },
   cardTitleWhite: {
     color: '#FFFFFF',
@@ -41,23 +115,75 @@ const styles = createStyles({
     }
   }
 });
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+// const isLoginInfo = (response: ReactFacebookLoginInfo | ReactFacebookFailureResponse): response is ReactFacebookLoginInfo =>
+
+const DialogTitle = withStyles(connectToSocialAccountsStyle)(
+  (props: DialogTitleProps) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+      <MuiDialogTitle disableTypography className={classes.root} {...other}>
+        <Typography variant="h6">{children}</Typography>
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            className={classes.closeButton}
+            onClick={onClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </MuiDialogTitle>
+    );
+  }
+);
 
 function SocialAccounts(props: any) {
   const { classes } = props;
   const { data, isLoading, error } = useGetMyProfileQuery();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const connectToSocialAccounts = (event: React.MouseEvent<HTMLElement>) => {
+    setOpen(true);
+  };
+  const callback = (
+    response: ReactFacebookLoginInfo | ReactFacebookFailureResponse
+  ) => {};
   return (
     <GridContainer>
       <GridItem xs={12} sm={12} md={12}>
         <Card>
           <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>
-              Connected Social Accounts
-            </h4>
-            <p className={classes.cardCategoryWhite}>
-              Here is a list of all connected social accounts for
-              {!isLoading && data && <Info>{data.company?.companyName}</Info>}
-              {error && <Info>N/A</Info>}
-            </p>
+            <div className={classes.cardTitle}>
+              <h4 className={classes.cardTitleWhite}>
+                Connected Social Accounts
+              </h4>
+              <div className={classes.cardCategoryWhite}>
+                Here is a list of all connected social accounts for
+                {!isLoading && data && <div>{data.company?.companyName}</div>}
+                {error && <div>N/A</div>}
+              </div>
+            </div>
+            <div className={classes.cardAction}>
+              <Tooltip title="Connect to Social Account">
+                <IconButton
+                  aria-label="connect to social accounts"
+                  onClick={connectToSocialAccounts}
+                >
+                  <AddIcon style={{ color: purple[50] }} />
+                </IconButton>
+              </Tooltip>
+            </div>
           </CardHeader>
           <CardBody>
             <Table
@@ -68,6 +194,38 @@ function SocialAccounts(props: any) {
           </CardBody>
         </Card>
       </GridItem>
+      <Dialog
+        open={open}
+        fullWidth={true}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="connect-social-account-title" onClose={handleClose}>
+          {'Connect to Social Account'}
+        </DialogTitle>
+        <DialogContent dividers>
+          <List className={classes.root} aria-label="contacts">
+            <ListItem>
+              <div id="facebookWrapper" className={classes.facebookIconStyle}>
+                <ReactFacebookLogin
+                  appId="1088597931155576"
+                  callback={callback}
+                  textButton="Sign in with Facebook"
+                  icon={<FacebookIcon />}
+                />
+              </div>
+            </ListItem>
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
     </GridContainer>
   );
 }

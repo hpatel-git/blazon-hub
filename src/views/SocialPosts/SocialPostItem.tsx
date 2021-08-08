ae4ef54253f364f66ce7674be82eb6f5b49a3245
hardikkumar <hardikkumar@datazoom.io>
2021-08-08 12:32:44 -0500

    Adding Page sync

--- src/views/SocialPosts/SocialPostItem.tsx
+++ src/views/SocialPosts/SocialPostItem.tsx
@@ -7,6 +7,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { formatDistance } from 'date-fns';
 import Typography from '@material-ui/core/Typography';
 import Linkify from 'linkifyjs/react';
+import { Box } from '@material-ui/core';
 import CardHeader from '@material-ui/core/CardHeader';
 import Grid from '@material-ui/core/Grid';
 import { Avatar, IconButton } from '@material-ui/core';
@@ -14,12 +15,11 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     media: {
       height: 0,
-      width: 300,
-      paddingTop: '45%', // 16:9,
-      marginTop: '30'
+      paddingTop: '56.25%', // 16:9
+      backgroundColor: 'white'
     },
     detailWrapper: {
-      margin: theme.spacing(3)
+      marginLeft: theme.spacing(3)
     },
     root: {
       flex: 1,
@@ -28,8 +28,11 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       fontWeight: 'bold'
     },
+    message: {
+      whiteSpace: 'pre-line',
+      margin: theme.spacing(2)
+    },
     description: {
-      width: 300,
       textOverflow: 'ellipsis',
       overflow: 'hidden',
       whiteSpace: 'nowrap'
@@ -49,74 +52,108 @@ interface SocialPostItemProps {
 export default function SocialPostItem(props: SocialPostItemProps) {
   const { ogImage, ogTitle, ogDescription, ogSiteName, message } = props;
   const isScraperDisplayRequired = () => {
+    let isDisplayRequired = true;
     if (
       ogImage === undefined &&
       ogTitle === undefined &&
       ogDescription === undefined &&
       ogSiteName === undefined
     ) {
-      return false;
+      isDisplayRequired = false;
     }
-    return true;
+    if (
+      ogImage === '' &&
+      ogTitle === '' &&
+      ogDescription === '' &&
+      ogSiteName === ''
+    ) {
+      isDisplayRequired = false;
+    }
+    console.log(
+      'ogImage:',
+      ogImage,
+      'ogTitle:',
+      ogTitle,
+      'ogDescription:',
+      ogDescription,
+      ' ogSiteName: ',
+      ogSiteName
+    );
+    return isDisplayRequired;
   };
   const classes = useStyles();
+  const defaultProps = {
+    bgcolor: '#D5D9DC',
+    borderColor: '#D5D9DC',
+    m: 1,
+    border: 1,
+    shadows: 12
+  };
 
   return (
     <React.Fragment>
-      {isScraperDisplayRequired() && (
-        <Card className={classes.root}>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="recipe" className={classes.avatar}>
-                S
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title="Saleseaze Post Preview"
-            subheader={formatDistance(new Date(), new Date(), {
-              addSuffix: true
-            })}
-          />
-          <CardContent>
-            <Grid container direction="column">
-              <Grid item xs={12}>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  style={{ whiteSpace: 'pre-line' }}
-                  paragraph={true}
-                  component="p"
-                >
-                  <Linkify>{message}</Linkify>
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                {ogImage && (
-                  <CardMedia
-                    className={classes.media}
-                    image={ogImage}
-                    title={ogImage}
-                  />
-                )}
-              </Grid>
-
-              <Grid item xs={12}>
-                <div className={classes.detailWrapper}>
-                  <div>{ogSiteName ? ogSiteName : ''}</div>
-                  <div className={classes.title}>{ogTitle ? ogTitle : ''}</div>
-                  <div className={classes.description}>
-                    {ogDescription ? ogDescription : ''}
-                  </div>
-                </div>
-              </Grid>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              S
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Saleseaze Post Preview"
+          subheader={formatDistance(new Date(), new Date(), {
+            addSuffix: true
+          })}
+        />
+        <CardContent>
+          <Grid container direction="column">
+            <Grid item xs={12}>
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                className={classes.message}
+                paragraph={true}
+                component="p"
+              >
+                <Linkify>{message}</Linkify>
+              </Typography>
             </Grid>
-          </CardContent>
-        </Card>
-      )}
+            {isScraperDisplayRequired() && (
+              <Grid item xs={12}>
+                <Box borderRadius="borderRadius" {...defaultProps}>
+                  <Grid container>
+                    {ogImage && (
+                      <Grid item xs={12}>
+                        <CardMedia
+                          className={classes.media}
+                          image={ogImage}
+                          title={ogImage}
+                        />
+                      </Grid>
+                    )}
+
+                    <Grid item xs={12}>
+                      <div className={classes.detailWrapper}>
+                        <div>{ogSiteName ? ogSiteName : ''}</div>
+                        <div className={classes.title}>
+                          {ogTitle ? ogTitle : ''}
+                        </div>
+                        <div className={classes.description}>
+                          {ogDescription ? ogDescription : ''}
+                        </div>
+                      </div>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
+            )}
+          </Grid>
+        </CardContent>
+      </Card>
     </React.Fragment>
   );
 }
